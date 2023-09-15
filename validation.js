3e274a237a2faff0e69a94b6808d23264d479ffa
MOHAMMED-JAHID <73962889+MOHAMMED-JAHID@users.noreply.github.com>
2023-09-15 14:08:49 +0100

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,32 @@
+// document.addEventListener('DOMContentLoaded',function() {
+// function showmenu(){
+
+// const menubutton =document.querySelector('.addmenu'),
+// closebutton=document.querySelector('.closebutton'),
+// addclass=document.querySelector('#hidemenu');
+//     try{
+//     menubutton.addEventListener('click', function(){
+//     addclass.classList.toggle('showmenu')
+//     })
+//     }catch(error){
+//     console.error('Error:' + error.message);
+//     }
+//     try{
+//     closebutton.addEventListener('click', function(){
+//     addclass.classList.remove('showmenu')
+//     })
+//     }catch(error){
+//     console.error('Error:' + error.message);
+//     }
+
+// let nav = document.querySelector('.nav');
+// let menu = document.querySelector('.addmenu');
+//     try{
+//     menu.innerHTML = nav.innerHTML;
+//     }catch(error){
+//         console.error('Error:' + error.message);
+//     }
+    
+//     }
+//     showmenu();
+//     });
